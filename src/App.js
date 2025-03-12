266e6d756d0cfbe9818bc9a7444012f22a4f7f7f
tharani3920 <122532168+tharani3920@users.noreply.github.com>
2025-03-12 15:29:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,18 +28,18 @@ import logo from './logo.svg';
    return (
      <div className="App">
        <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
+         // <img src={logo} className="App-logo" alt="logo" />
+         // <p>
+         //   Edit <code>src/App.js</code> and save to reload.
+         // </p>
+         // <a
+         //   className="App-link"
+         //   href="https://reactjs.org"
+         //   target="_blank"
+         //   rel="noopener noreferrer"
+         // >
+         //   Learn React
+         // </a>
          <div className="fan-div" ref={fanRef}>
            <div className="rectangle one"></div>
            <div className="all-in-one">
